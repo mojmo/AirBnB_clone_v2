c6eea604916626c2b3268b862ddc18e803bafd74
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-01-13 13:51:44 +0200

    update docstring for storage __init__ file and remove base model test

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-""""""
+"""
+This script initializes an instance of the FileStorage class from the
+'models.engine.file_storage' module and reloads previously stored data.
+"""
 from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
