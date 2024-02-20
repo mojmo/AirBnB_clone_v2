c808d7181aa394ba0eb8d17ab460be95f42ee347
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-02-21 00:06:48 +0200

    remove is and change it with ==

--- models/city.py
+++ models/city.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """Defines the City class as a subclass of BaseModel."""
-from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+
 from models import storage_type
+from models.base_model import BaseModel, Base
 
 
 class City(BaseModel, Base):
     """Represents a city entity."""
 
-    if storage_type is 'db':
+    if storage_type == 'db':
         __tablename__ = 'cities'
-        name =  Column(String(128), nullable=False)
+        name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     else:
         name = ''
