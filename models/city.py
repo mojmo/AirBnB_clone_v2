097a7c837f17436289663ce0961f29a1fa6d8e4e
mojtaba <mojtaba.mohamed187@gmail.com>
2024-02-20 23:39:05 +0200

    update city and state classes to deal with DB

--- models/city.py
+++ models/city.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """Defines the City class as a subclass of BaseModel."""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """Represents a city entity."""
 
-    state_id = ''
-    name = ''
+    __tablename__ = 'cities'
+    name =  Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
--- models/state.py
+++ models/state.py
@@ -1,9 +1,31 @@
 #!/usr/bin/python3
 """Defines the State class as a subclass of BaseModel."""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models import storage, storage_type
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """Represents a state entity."""
 
-    name = ''
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+    cities = relationship("City", backref="state", cascade="all, delete")
+
+    @property
+    def cities(self):
+        """Return a list of City instances associated with this State.
+
+        Returns:
+            list: A list of City instances that have the same
+            state_id as this State's id.
+        """
+        cities_list = []
+
+        if storage_type is not 'db':
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    cities_list.append(city)
+            return cities_list
