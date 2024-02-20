ddf01b9cb376ac1ac499f3acd65d4d77ed6c158e
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-02-20 23:02:15 +0200

    Add a conditional depending of the value of the environment variable HBNB_TYPE_STORAGE: If equal to db: Import DBStorage class in this file Create an instance of DBStorage and store it in the variable storage (the line storage.reload() should be executed after this instantiation) Else: Import FileStorage class in this file Create an instance of FileStorage and store it in the variable storage (the line storage.reload() should be executed after this instantiation) This “switch” will allow you to change storage type directly by using an environment variable (example below)

--- models/__init__.py
+++ models/__init__.py
@@ -3,8 +3,16 @@
 This script initializes an instance of the FileStorage class from the
 'models.engine.file_storage' module and reloads previously stored data.
 """
-from models.engine.file_storage import FileStorage
+from os import getenv
 
-storage = FileStorage()
 
+storage_type = getenv('HBNB_TYPE_STORAGE')
+
+if storage_type == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 storage.reload()
+
