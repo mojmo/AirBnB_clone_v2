eba4b6eb907a7cfbe841dfad4eba1ffab3b1dae7
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-02-22 00:10:35 +0200

    fix id not found

--- models/base_model.py
+++ models/base_model.py
@@ -44,6 +44,8 @@ class BaseModel:
                             datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
                 else:
                     setattr(self, key, value)
+            if kwargs.get("id", None) is None:
+                self.id = str(uuid.uuid4())
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
