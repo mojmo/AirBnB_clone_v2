349327fef3e776cb0cdbc34ce26c7cfe5b5fa41f
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-02-21 03:12:11 +0200

    Update User: (models/user.py)
    User inherits from BaseModel and Base (respect the order)
    Add or replace in the class User:
    class attribute __tablename__
    represents the table name, users
    class attribute email
    represents a column containing a string (128 characters)
    can’t be null
    class attribute password
    represents a column containing a string (128 characters)
    can’t be null
    class attribute first_name
    represents a column containing a string (128 characters)
    can be null
    class attribute last_name
    represents a column containing a string (128 characters)
    can be null

--- models/user.py
+++ models/user.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python3
-"""Defines the User class as a subclass of BaseModel."""
-from models.base_model import BaseModel
+""" holds class User"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
-    """Represents a user entity."""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """Representation of a user """
+    if models.storage_type == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
