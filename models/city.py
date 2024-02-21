0d0e3a5c9ecfe8435b64e9de442d70fc7beccd9d
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-02-21 06:52:54 +0200

    add places field  in city, and in user <linking between tables>
    add place table

--- models/city.py
+++ models/city.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines the City class as a subclass of BaseModel."""
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 import models
 from models import storage_type
@@ -13,6 +14,7 @@ class City(BaseModel, Base):
         __tablename__ = 'cities'
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="cities")
     else:
         state_id = ""
         name = ""
--- models/user.py
+++ models/user.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """ holds class User"""
+from sqlalchemy.orm import relationship
+
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -16,6 +17,7 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
     else:
         email = ""
         password = ""
