7d70d61c5644fe3fba0ce56f281392e2c26cd3de
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-02-21 06:23:16 +0200

    Add import user

--- models/engine/__init__.py
+++ models/engine/__init__.py
@@ -3,3 +3,7 @@
 
 from models.city import City
 from models.state import State
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
+from models.user import User
