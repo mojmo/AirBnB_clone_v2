a1abca9bb412c6daf58def8e0f51b6a1baf34f7e
mojtaba <mojtaba.mohamed187@gmail.com>
2024-02-22 03:21:21 +0200

    fix test_state

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,227 +1,92 @@
-"""Unit tests for the State class."""
-import unittest
-import os
+#!/usr/bin/python3
+"""
+Contains the TestStateDocs classes
+"""
 
-from models.state import State
 from datetime import datetime
-from time import sleep
+import inspect
+import models
+from models import state
+from models.base_model import BaseModel
+import pep8
+import unittest
+State = state.State
 
-condition = os.getenv('HBNB_TYPE_STORAGE') != 'db'
+
+class TestStateDocs(unittest.TestCase):
+    """Tests to check the documentation and style of State class"""
+    @classmethod
+    def setUpClass(cls):
+        """Set up for the doc tests"""
+        cls.state_f = inspect.getmembers(State, inspect.isfunction)
+
+    def test_pep8_conformance_state(self):
+        """Test that models/state.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['models/state.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_state_module_docstring(self):
+        """Test for the state.py module docstring"""
+        self.assertIsNot(state.__doc__, None,
+                         "state.py needs a docstring")
+        self.assertTrue(len(state.__doc__) >= 1,
+                        "state.py needs a docstring")
+
+    def test_state_class_docstring(self):
+        """Test for the State class docstring"""
+        self.assertIsNot(State.__doc__, None,
+                         "State class needs a docstring")
+        self.assertTrue(len(State.__doc__) >= 1,
+                        "State class needs a docstring")
+
+    def test_state_func_docstrings(self):
+        """Test for the presence of docstrings in State methods"""
+        for func in self.state_f:
+            self.assertIsNot(func[1].__doc__, None,
+                             "{:s} method needs a docstring".format(func[0]))
+            self.assertTrue(len(func[1].__doc__) >= 1,
+                            "{:s} method needs a docstring".format(func[0]))
 
 
-@unittest.skipIf(condition, "Reason for skipping the tests")
 class TestState(unittest.TestCase):
-    """Contains unit tests for the State class."""
-
-    def test_name_class_attribute(self):
-        """
-        Test that the State has a class attribute 'name'
-        """
+    """Test the State class"""
+    def test_is_subclass(self):
+        """Test that State is a subclass of BaseModel"""
         state = State()
-        self.assertEqual(type(State.name), str)
-        self.assertIn("name", dir(state))
-        self.assertNotIn("name", state.__dict__)
+        self.assertIsInstance(state, BaseModel)
+        self.assertTrue(hasattr(state, "id"))
+        self.assertTrue(hasattr(state, "created_at"))
+        self.assertTrue(hasattr(state, "updated_at"))
 
-    #  State object can be instantiated with no arguments
-    def test_instantiate_with_no_arguments(self):
-        """
-        Test that a State object can be instantiated
-        with no arguments.
-        """
+    def test_name_attr(self):
+        """Test that State has attribute name, and it's as an empty string"""
         state = State()
-        self.assertIsInstance(state, State)
+        self.assertTrue(hasattr(state, "name"))
+        if models.storage_type == 'db':
+            self.assertEqual(state.name, None)
+        else:
+            self.assertEqual(state.name, "")
 
-    #  State object can be instantiated with name argument
-    def test_instantiate_with_name_argument(self):
-        """
-        Test that a State object can be instantiated
-        with a name argument.
-        """
-        state = State(name="California")
-        self.assertIsInstance(state, State)
-        self.assertEqual(state.name, "California")
+    def test_to_dict_creates_dict(self):
+        """test to_dict method creates a dictionary with proper attrs"""
+        s = State()
+        new_d = s.to_dict()
+        self.assertEqual(type(new_d), dict)
+        self.assertFalse("_sa_instance_state" in new_d)
+        for attr in s.__dict__:
+            if attr is not "_sa_instance_state":
+                self.assertTrue(attr in new_d)
+        self.assertTrue("__class__" in new_d)
 
-    #  State object name can be set to an empty string
-    def test_set_name_to_empty_string(self):
-        """
-        Test that the name of a State object can be
-        set to an empty string.
-        """
-        state = State()
-        state.name = ""
-        self.assertEqual(state.name, "")
-
-    #  State object name can be set to a string with length 128
-    def test_set_name_to_string_with_length_128(self):
-        """
-        Test that the name of a State object can be
-        set to a string with a length of 128.
-        """
-        state = State()
-        state.name = "A" * 128
-        self.assertEqual(state.name, "A" * 128)
-
-    #  State object name can be set to a string with length greater than 128
-    def test_set_name_to_string_with_length_greater_than_128(self):
-        """
-        Test that the name of a State object can be set to
-        a string with a length greater than 128.
-        """
-        state = State()
-        state.name = "A" * 129
-        self.assertEqual(state.name, "A" * 129)
-
-@unittest.skipIf(condition, "Reason for skipping the tests")
-class TestStateInit(unittest.TestCase):
-    """Contains unit tests for the State class."""
-
-    def tearDown(self):
-        """Delete any created files during testing."""
-        try:
-            os.remove("file.json")
-        except IOError:
-            pass
-
-    def test_state_instance_no_arguments(self):
-        """
-        Test that a state instance can be created with no arguments
-        """
-        state = State()
-        self.assertIsInstance(state, State)
-
-    def test_state_args_types(self):
-        """Tests the types of attributes after state instantiation"""
-        obj = State()
-        self.assertIsInstance(obj.id, str)
-        self.assertIsInstance(obj.created_at, datetime)
-        self.assertIsInstance(obj.updated_at, datetime)
-
-    def test_state_instance_keyword_arguments(self):
-        """
-        Test that a state instance can be created with keyword arguments
-        """
-        state = State(created_at='2022-01-01T00:00:00.000')
-        self.assertIsInstance(state, State)
-        self.assertEqual(state.created_at, datetime(2022, 1, 1, 0, 0, 0))
-
-    def test_state_init_with_kwargs(self):
-        """Tests state initialization with keyword arguments"""
-        data = {'id': '415b1fec-2336-4dfa-8254-3b9006da20f1',
-                'created_at': '2024-01-14T12:00:00.000'}
-        obj = State(**data)
-
-        self.assertEqual(obj.id, '415b1fec-2336-4dfa-8254-3b9006da20f1')
-        self.assertEqual(obj.created_at, datetime(2024, 1, 14, 12, 0, 0))
-
-    def test_base_moder_not_used_args(self):
-        """Tests state instantiation with unused positional arguments"""
-        state = State("415b1fec-2336-4dfa-8254-3b9006da20e3")
-
-        self.assertIsInstance(state, State)
-        self.assertNotEqual(state.id,
-                            "415b1fec-2336-4dfa-8254-3b9006da20e3")
-
-    def test_state_with_None(self):
-        """Tests state instantiation with None as an argument"""
-        state = State(None)
-        self.assertIsInstance(state, State)
-
-    def test_state_str(self):
-        """Tests the string representation of state instances"""
-
-        obj = State()
-        obj.id = "415swfec-4536-4dfa-8254-3b9006da20fa"
-        date = datetime.now()
-        obj.created_at = obj.updated_at = date
-        output = str(obj)
-
-        self.assertIn("[State] (415swfec-4536-4dfa-8254-3b9006da20fa)",
-                      output)
-        self.assertIn("'id': '415swfec-4536-4dfa-8254-3b9006da20fa'", output)
-        self.assertIn(f"'created_at': {repr(date)}", output)
-        self.assertIn(f"'updated_at': {repr(date)}", output)
-
-    def test_state_instance_invalid_created_at(self):
-        """
-        Test that a state instance cannot be created with an
-        invalid created_at
-        """
-        with self.assertRaises(ValueError):
-            State(created_at='invalid_date')
-
-@unittest.skipIf(condition, "Reason for skipping the tests")
-class TeststateSave(unittest.TestCase):
-    """Contains tests related to the save method of State instances"""
-
-    def tearDown(self):
-        """Delete any created files during testing."""
-        try:
-            os.remove("file.json")
-        except IOError:
-            pass
-
-    def test_save_method(self):
-        """Tests the save method of state instances"""
-        obj = State()
-        old_updated_at = obj.updated_at
-        sleep(0.1)
-        obj.save()
-        self.assertGreater(obj.updated_at, old_updated_at)
-
-    def test_save_two_times(self):
-        """
-        Tests calling the save method twice and checking
-        if updated_at is greater the second time
-        """
-        obj = State()
-        sleep(0.1)
-        first_updated_at = obj.updated_at
-        obj.save()
-        sleep(0.1)
-        obj.save()
-        second_updated_at = obj.updated_at
-        self.assertGreater(second_updated_at, first_updated_at)
-
-    def test_save_with_arg(self):
-        """
-        Tests calling the save method with an argument
-        (expects a TypeError).
-        """
-        obj = State()
-        old_updated_at = obj.updated_at
-        sleep(0.1)
-        with self.assertRaises(TypeError):
-            obj.save(None)
-
-@unittest.skipIf(condition, "Reason for skipping the tests")
-class TeststateToDict(unittest.TestCase):
-    """Contains tests related to the to_dict method of State instances"""
-
-    def tearDown(self):
-        """Delete any created files during testing."""
-        try:
-            os.remove("file.json")
-        except IOError:
-            pass
-
-    def test_to_dict_method(self):
-        """Tests the to_dict method of state instances"""
-        obj = State()
-        self.assertIsInstance(obj.to_dict(), dict)
-
-    def test_to_dict_contents(self):
-        """Tests the contents of the dictionary returned by to_dict"""
-        obj = State()
-        obj.id = "99283s39sn2v"
-        new_dict = obj.to_dict()
-        self.assertEqual(new_dict['id'], "99283s39sn2v")
-        self.assertEqual(new_dict['__class__'], "State")
-
-    def test_to_dict_with_arg(self):
-        """
-        Tests calling the to_dict method with an argument
-        (expects a TypeError)
-        """
-        obj = State()
-        with self.assertRaises(TypeError):
-            obj.to_dict(None)
+    def test_to_dict_values(self):
+        """test that values in dict returned from to_dict are correct"""
+        t_format = "%Y-%m-%dT%H:%M:%S.%f"
+        s = State()
+        new_d = s.to_dict()
+        self.assertEqual(new_d["__class__"], "State")
+        self.assertEqual(type(new_d["created_at"]), str)
+        self.assertEqual(type(new_d["updated_at"]), str)
+        self.assertEqual(new_d["created_at"], s.created_at.strftime(t_format))
+        self.assertEqual(new_d["updated_at"], s.updated_at.strftime(t_format))
