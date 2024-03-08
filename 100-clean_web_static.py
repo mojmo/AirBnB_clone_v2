2b8b727a855309e695687c747abf1b0d3342005d
mojtaba <mojtaba.mohamed187@gmail.com>
2024-03-08 03:59:24 +0200

    Keep it clean

---
+++ 100-clean_web_static.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Cleans up old archives and releases from the server."""
+
+import os
+from fabric.api import env, run, cd
+
+env.hosts = ['3.83.253.65', '3.84.239.237']
+
+
+def do_clean(number=0):
+    """Cleans up old archives and releases from the server."""
+
+    # Convert number to an integer, default to 1 if 0 or invalid
+    number = max(1, int(number))
+
+    # Remove old archives
+    archives_path = "versions"
+    archives = sorted(os.listdir(archives_path))
+    for archive in archives[:-number]:
+        os.remove(os.path.join(archives_path, archive))
+
+    # Remove old releases
+    releases_path = "/data/web_static/releases"
+    with cd(releases_path):
+        releases = run("ls -tr | grep 'web_static_'").split()
+        for release in releases[:-number]:
+            run(f"rm -rf {release}")
