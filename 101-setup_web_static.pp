58c14b266c14d943a3b87b81ba33f48c93bd229c
mojtaba <mojtaba.mohamed187@gmail.com>
2024-03-08 04:52:09 +0200

    connect manifests

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,12 +1,12 @@
 # Install Nginx package
 package { 'nginx':
   ensure => installed,
-}
+} ->
 
 # Create necessary directories
 file { ['/data/web_static/releases/test', '/data/web_static/shared']:
   ensure => directory,
-}
+} ->
 
 # Create a fake HTML file
 file { '/data/web_static/releases/test/index.html':
@@ -18,20 +18,20 @@ file { '/data/web_static/releases/test/index.html':
                     Holberton School
                 </body>
             </html>',
-}
+} ->
 
 # Create or recreate the symbolic link
 file { '/data/web_static/current':
   ensure => link,
   target => '/data/web_static/releases/test',
-}
+} ->
 
 # Give ownership of /data folder to ubuntu user and group recursively
 file { '/data':
   owner   => 'ubuntu',
   group   => 'ubuntu',
   recurse => true,
-}
+} ->
 
 # Update Nginx configuration
 file_line { 'nginx_config':
@@ -41,7 +41,7 @@ file_line { 'nginx_config':
   match   => '^server {',
   after   => true,
   notify  => Service['nginx'],
-}
+} ->
 
 # Restart the Nginx service to apply the new configuration
 service { 'nginx':
