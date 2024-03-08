7a918c490ec83c3314e58c28e3aef58384fa6e94
mojtaba <mojtaba.mohamed187@gmail.com>
2024-03-08 04:58:50 +0200

    puppet config

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,50 +1,92 @@
-# Install Nginx package
+# Configures a web server for deployment of web_static.
+
+# Nginx configuration file
+$nginx_conf = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}"
+
 package { 'nginx':
-  ensure => installed,
+  ensure   => 'present',
+  provider => 'apt'
 } ->
 
-# Create necessary directories
-file { ['/data/web_static/releases/test', '/data/web_static/shared']:
-  ensure => directory,
-} ->
-
-# Create a fake HTML file
-file { '/data/web_static/releases/test/index.html':
-  ensure  => present,
-  content => '<html>
-                <head>
-                </head>
-                <body>
-                    Holberton School
-                </body>
-            </html>',
-} ->
-
-# Create or recreate the symbolic link
-file { '/data/web_static/current':
-  ensure => link,
-  target => '/data/web_static/releases/test',
-} ->
-
-# Give ownership of /data folder to ubuntu user and group recursively
 file { '/data':
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  recurse => true,
+  ensure  => 'directory'
 } ->
 
-# Update Nginx configuration
-file_line { 'nginx_config':
-  ensure  => present,
-  path    => '/etc/nginx/sites-available/default',
-  line    => 'location /hbnb_static { alias /data/web_static/current/; }',
-  match   => '^server {',
-  after   => true,
-  notify  => Service['nginx'],
+file { '/data/web_static':
+  ensure => 'directory'
 } ->
 
-# Restart the Nginx service to apply the new configuration
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
+file { '/data/web_static/releases':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases/test':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/shared':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases/test/index.html':
+  ensure  => 'present',
+  content => "ALX School\n"
+} ->
+
+file { '/data/web_static/current':
+  ensure => 'link',
+  target => '/data/web_static/releases/test'
+} ->
+
+exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
+}
+
+file { '/var/www':
+  ensure => 'directory'
+} ->
+
+file { '/var/www/html':
+  ensure => 'directory'
+} ->
+
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "Holberton School Nginx\n"
+} ->
+
+file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page\n"
+} ->
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => $nginx_conf
+} ->
+
+exec { 'restart_server':
+  provider  => 'shell',
+  command   => 'service ngingx restart'
 }
