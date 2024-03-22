0db665197161149e12b9334d1de68be0e4cabd80
mojtaba <mojtaba.mohamed187@gmail.com>
2024-03-22 14:38:13 +0200

    Add __init__.py file and first route

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""A simple Flask application with one route."""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """Display a greeting message."""
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
