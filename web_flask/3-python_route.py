5ae6a50ae7b98537433e24de2fb16192114101e5
mojtaba <mojtaba.mohamed187@gmail.com>
2024-03-22 14:40:11 +0200

    Add /python/<text> route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+"""A simple Flask application with various routes."""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """Display a greeting message."""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Display 'HBNB'."""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """Display 'C' followed by the value of <text>,
+    replacing underscores with spaces."""
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def py_text(text='is cool'):
+    """Display 'Python' followed by the value of <text>,
+    replacing underscores with spaces.
+
+    If no <text> is provided, default to 'is cool'.
+    """
+    return f"Python {text.replace('_', ' ')}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
